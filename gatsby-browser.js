dafe7f867580fc16d0e289f802debc71832c7565
alborz1031 <alborz1031@gmail.com>
2019-02-09 00:59:15 -0500

    ✨  Display message when service worker updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+
+// Display a message when the service worker updates
+exports.onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
